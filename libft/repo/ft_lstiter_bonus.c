18faef6fdc9ce1520949b6eee63aad68f200f084
ander-vieira <ander.vieira741@gmail.com>
2023-09-18 17:45:47 +0200

    libft: remove unnecessary check from ft_lstiter

--- libft/repo/ft_lstiter_bonus.c
+++ libft/repo/ft_lstiter_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstiter_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: andeviei <andeviei@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ander <ander@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 12:29:54 by andeviei          #+#    #+#             */
-/*   Updated: 2023/09/16 21:31:53 by andeviei         ###   ########.fr       */
+/*   Updated: 2023/09/18 17:32:11 by ander            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ void	ft_lstiter(t_list *lst, void (*fun)(void *))
 	if (lst != NULL)
 	{
 		fun(lst->content);
-		if (lst->next != NULL)
-			ft_lstiter(lst->next, fun);
+		ft_lstiter(lst->next, fun);
 	}
 }
