b0bed2029a28b4ed5c27b8e396b6d32e8efd5c96
ander-vieira <ander.vieira741@gmail.com>
2023-11-09 15:13:27 +0100

    so_long: add more tests

--- so_long/test.sh
+++ so_long/test.sh
@@ -10,11 +10,19 @@ make -C repo/ -s all
 echo "---------------"
 echo "TESTS"
 echo "---------------"
-echo "ERROR TEST 1:"
+echo "INCORRECT PARAMS TEST 1:"
 repo/so_long
-echo "ERROR TEST 2:"
+echo "INCORRECT PARAMS TEST 2:"
+repo/so_long a b
+echo "WRONG FILE EXTENSION TEST:"
 repo/so_long a
-echo "CORRECT CALL TEST:"
+echo "FILE NOT FOUND TEST:"
 repo/so_long a.ber
+echo "INCORRECT MAP TEST 1 (map_b1.ber):"
+repo/so_long repo/maps/map_b1.ber
+echo "INCORRECT MAP TEST 2 (map_b2.ber):"
+repo/so_long repo/maps/map_b2.ber
+echo "CORRECT TEST (map_g1.ber):"
+repo/so_long repo/maps/map_g1.ber
 
 make -C repo/ -s clean
