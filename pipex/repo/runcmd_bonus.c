cc2437faeb1f1cde4437680ada420d5ef79e7986
ander-vieira <ander.vieira741@gmail.com>
2023-11-26 18:12:36 +0100

    pipex: fix leaks and close fds, in bonus

--- pipex/repo/runcmd_bonus.c
+++ pipex/repo/runcmd_bonus.c
@@ -6,7 +6,7 @@
 /*   By: andeviei <andeviei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 14:00:09 by andeviei          #+#    #+#             */
-/*   Updated: 2023/11/25 00:23:11 by andeviei         ###   ########.fr       */
+/*   Updated: 2023/11/26 18:12:11 by andeviei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,9 @@ static t_bool	av_updatefdin(t_pipex *px, size_t i)
 			return (av_printerror(px->pname, px->infile, NULL), FALSE);
 	}
 	if (dup2(px->cmds[i].fd_in, STDIN_FILENO) == -1)
-		return (av_printerror(px->pname, "dup2", NULL), FALSE);
-	return (TRUE);
+		return (av_printerror(px->pname, "dup2", NULL),
+			close(px->cmds[i].fd_in), FALSE);
+	return (close(px->cmds[i].fd_in), TRUE);
 }
 
 static t_bool	av_updatefdout(t_pipex *px, size_t i)
@@ -47,8 +48,9 @@ static t_bool	av_updatefdout(t_pipex *px, size_t i)
 	if (px->cmds[i].fd_out == -1)
 		return (av_printerror(px->pname, px->outfile, NULL), FALSE);
 	if (dup2(px->cmds[i].fd_out, STDOUT_FILENO) == -1)
-		return (av_printerror(px->pname, "dup2", NULL), FALSE);
-	return (TRUE);
+		return (av_printerror(px->pname, "dup2", NULL),
+			close(px->cmds[i].fd_out), FALSE);
+	return (close(px->cmds[i].fd_out), TRUE);
 }
 
 static int	av_runcmd_child(t_pipex *px, size_t i)
@@ -57,18 +59,18 @@ static int	av_runcmd_child(t_pipex *px, size_t i)
 	char	*pname;
 
 	if (!av_updatefdin(px, i))
-		return (EXIT_FAILURE);
+		return (av_freepipex(px), EXIT_FAILURE);
 	if (!av_updatefdout(px, i))
-		return (EXIT_FAILURE);
+		return (av_freepipex(px), EXIT_FAILURE);
 	argv = av_getargv(px, i);
 	if (argv == NULL)
-		return (EXIT_FAILURE);
+		return (av_freepipex(px), EXIT_FAILURE);
 	pname = av_getpath(argv[0], px);
 	if (pname == NULL)
-		return (av_freeargv(argv), EXIT_FAILURE);
+		return (av_freeargv(argv), av_freepipex(px), EXIT_FAILURE);
 	execve(pname, argv, px->env);
 	av_printerror(px->pname, "execve", NULL);
-	return (EXIT_FAILURE);
+	return (free(pname), av_freeargv(argv), av_freepipex(px), EXIT_FAILURE);
 }
 
 t_bool	av_runcmd(t_pipex *px, size_t i)
