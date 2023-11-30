50739e9f5bf31e3832953c9b3f94299b61684652
ander-vieira <ander.vieira741@gmail.com>
2023-11-30 15:27:37 +0100

    pipex: remove av_putstr

--- pipex/repo/error_bonus.c
+++ pipex/repo/error_bonus.c
@@ -6,43 +6,37 @@
 /*   By: andeviei <andeviei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 17:21:26 by andeviei          #+#    #+#             */
-/*   Updated: 2023/11/25 00:22:42 by andeviei         ###   ########.fr       */
+/*   Updated: 2023/11/30 15:26:41 by andeviei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
 
-static void	av_putstr(char *str, int fd)
-{
-	write(fd, str, ft_strlen(str));
-}
-
 void	av_printusage(char *pname)
 {
-	av_putstr("Usage: \n", STDOUT_FILENO);
-	av_putstr(pname, STDOUT_FILENO);
-	av_putstr(" <infile> <cmd1> <...> <cmdN> <outfile>\n",
-		STDOUT_FILENO);
-	av_putstr(pname, STDOUT_FILENO);
-	av_putstr(" here_doc <end sequence> <cmd1> <...> <cmdN> <outfile>\n",
-		STDOUT_FILENO);
+	ft_printstr(STDOUT_FILENO, "Usage: \n");
+	ft_printstr(STDOUT_FILENO, pname);
+	ft_printstr(STDOUT_FILENO, " <infile> <cmd1> <...> <cmdN> <outfile>\n");
+	ft_printstr(STDOUT_FILENO, pname);
+	ft_printstr(STDOUT_FILENO, \
+		" here_doc <end sequence> <cmd1> <...> <cmdN> <outfile>\n");
 }
 
 void	av_printerror(char *pname, char *func, char *msg)
 {
 	if (pname != NULL)
 	{
-		av_putstr(pname, STDERR_FILENO);
-		av_putstr(": ", STDERR_FILENO);
+		ft_printstr(STDERR_FILENO, pname);
+		ft_printstr(STDERR_FILENO, ": ");
 	}
 	if (func != NULL)
 	{
-		av_putstr(func, STDERR_FILENO);
-		av_putstr(": ", STDERR_FILENO);
+		ft_printstr(STDERR_FILENO, func);
+		ft_printstr(STDERR_FILENO, ": ");
 	}
 	if (msg != NULL)
-		av_putstr(msg, STDERR_FILENO);
+		ft_printstr(STDERR_FILENO, msg);
 	else
-		av_putstr(strerror(errno), STDERR_FILENO);
-	av_putstr("\n", STDERR_FILENO);
+		ft_printstr(STDERR_FILENO, strerror(errno));
+	ft_printstr(STDERR_FILENO, "\n");
 }
