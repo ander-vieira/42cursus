0394d835a3eb63a268a9ca2614ab3ba8dfcf4e7c
ander-vieira <ander.vieira741@gmail.com>
2023-12-05 17:09:10 +0100

    mylibft: add more gnl tests

--- mylibft/tests/test_gnl.c
+++ mylibft/tests/test_gnl.c
@@ -6,7 +6,7 @@
 /*   By: andeviei <andeviei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 16:53:52 by andeviei          #+#    #+#             */
-/*   Updated: 2023/12/05 17:00:56 by andeviei         ###   ########.fr       */
+/*   Updated: 2023/12/05 17:08:40 by andeviei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,11 @@
 void	test_gnl1(void)
 {
 	t_fd	fd[2];
-	char	*str;
 	char	*line;
 	t_error	error;
 
-	str = "HOLA\nSOY ANDER\nY TU?";
 	pipe(fd);
-	write(fd[1], str, strlen(str));
+	write(fd[1], "HOLA\nSOY ANDER\nY TU?", 20);
 	close(fd[1]);
 	line = ft_gnl(fd[0], &error);
 	print_test("1", !strcmp(line, "HOLA\n") && error == ERR_OK, FALSE);
@@ -35,8 +33,10 @@ void	test_gnl1(void)
 	line = ft_gnl(fd[0], &error);
 	print_test("4", line == NULL && error == ERR_OK, FALSE);
 	close(fd[0]);
+	line = ft_gnl(fd[0], &error);
+	print_test("5", line == NULL && error == ERR_READ, FALSE);
 	line = ft_gnl(-1, &error);
-	print_test("5", line == NULL && error == ERR_GNL_BADFD, TRUE);
+	print_test("6", line == NULL && error == ERR_GNL_BADFD, TRUE);
 }
 
 void	test_gnl(void)
