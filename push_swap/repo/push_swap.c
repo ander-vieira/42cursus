029bdae3057ea0366992ae1b4794a77b5c5c495f
ander-vieira <ander.vieira741@gmail.com>
2024-02-09 17:05:01 +0100

    push_swap: begin project

---
+++ push_swap/repo/push_swap.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andeviei <andeviei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/09 16:56:06 by andeviei          #+#    #+#             */
+/*   Updated: 2024/02/09 17:04:29 by andeviei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	ft_printf(STDOUT_FILENO, "push_swap\n");
+	return (EXIT_SUCCESS);
+}
