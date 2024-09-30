6615de8ee1463439867c5d198d13c44a59b1a273
ander-vieira <ander.vieira741@gmail.com>
2024-09-30 19:06:54 +0200

    CPP03: exercise 0

---
+++ cpp/m03/repo/ex00/ClapTrap.hpp
@@ -0,0 +1,23 @@
+#ifndef CLAPTRAP_H
+# define CLAPTRAP_H
+
+# include <iostream>
+
+class ClapTrap {
+	public:
+		ClapTrap();
+		ClapTrap(std::string const name);
+		ClapTrap(ClapTrap const &clapTrap);
+		ClapTrap &operator=(ClapTrap const &clapTrap);
+		~ClapTrap();
+		void attack(std::string const &target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+	private:
+		std::string name;
+		int hitPoints;
+		int energyPoints;
+		int attackDamage;
+};
+
+#endif
---
+++ cpp/m03/repo/ex00/main.cpp
@@ -0,0 +1,25 @@
+#include "ClapTrap.hpp"
+
+int main() {
+	ClapTrap clapTrap1("Alpha"), clapTrap2("Beta");
+
+	clapTrap1.attack("1");
+	clapTrap1.attack("2");
+	clapTrap1.attack("3");
+	clapTrap1.attack("4");
+	clapTrap1.attack("5");
+	clapTrap1.attack("6");
+	clapTrap1.attack("7");
+	clapTrap1.attack("8");
+	clapTrap1.attack("9");
+	clapTrap1.attack("10");
+	clapTrap1.attack("11");
+
+	clapTrap2.attack("Alpha");
+
+	clapTrap1.takeDamage(0);
+
+	clapTrap2.beRepaired(2);
+
+	return 0;
+}
